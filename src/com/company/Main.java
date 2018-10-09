89a472c73d979c0dc1da636b1edfb35e0db32d67
GBTC406005UR <GBTC406005UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-09 16:50:14 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+//9. Given array 1: [1,7,6,5,9] and array 2: [2,7,6,3,4]
+//Write a program that will print out all matching pairs from arrays 1 and 2.
+//Expected output:
+//(7,7) and (6,6)
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int []array1 = {1,7,6,5,9};
+        int []array2 = {2,7,6,3,4};
+    }
+    for (int i)
+
+
+}
